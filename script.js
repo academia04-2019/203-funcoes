2dc1ed15ae2374b58e6953a1d46dc4a9dd127dc6
Lucas Melo <luucasmell@gmail.com>
2019-06-12 16:38:27 -0300

    Adicionar anotacoes sobre funcao

---
+++ script.js
@@ -0,0 +1,83 @@
+
+// let nome = 'Lucas';
+// let cargo = 'programador';
+// let signo = 'aries';
+
+// alert(`O nome do usuario é: ${nome} e o cargo dele é: ${cargo} e o signo é ${signo}`);
+
+//CONSTRUIR UMA ESCADA NO CONSOLE
+//O MATERIAL DA ESCADA DEVE SER #
+//GERAR A QNTD DE DEGRAUS CONFORME PEDIDO PELO USUARIO
+// FOR NORMAL É MAIS FACIL
+
+
+// idade = idade + 1
+// idade += 1
+
+// SEGUNDA ETAPA
+// PERGUNTAR QUAL É O MATERIAL QUE O USUARIO QUER
+
+// let materialEscada = prompt("Digite o material da escada");
+// let degrau = materialEscada;
+// let qntd= Number(prompt("Opa, quantos degraus?"));
+
+// for(let i=0; i<qntd; i++){
+//   console.log(materialEscada);
+//   materialEscada += degrau;
+// }
+
+
+// let lista = ["batata", "pastel", "n gosto de tomate", "nem de pudim"];
+
+// let objeto={
+//     casa:[],
+//     carro:[]   
+// }
+
+// let filmes = [
+//   {titulo:"Harry Potter", idade: 10, ator: "Daniel"},
+//   {titulo:"It, a coisa", idade: 16},
+//   {titulo:"O albergue", idade: 18}
+// ]
+
+//TEM Q USAR FOR GENTE ! PF ! VALE MEIO NA MÉDIA 
+
+// USA IF DENTRO DE UM FOR
+
+
+// BEM VINDOS A FUNCOES
+
+
+// function somar(n1, n2, n3, n4){
+//   let media = (n1+n2+n3+n4)/4;
+//   console.log(`A media da Dandara é: ${media}`);
+// }
+
+// somar(9, 8, 7, 10);
+
+
+// function calc(a,b,c){
+//   console.log(a,b,c);
+// }
+// calc(1,'/',2);
+
+
+// function calculadora(a,b){
+//     console.log(a+b)
+// }
+
+// calculadora = (a,b) =>{
+//   console.log(a+b)
+// }
+
+
+// let fazerConta = function(a,b){
+//     console.log(a+b)
+// }
+
+function calculadora(a,b){
+  console.log(a+b);
+}
+calculadora(1,2);
+
+console.log(1+2);
